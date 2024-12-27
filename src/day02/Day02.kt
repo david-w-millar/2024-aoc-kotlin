cd409e19d7f27b7cd5f2e772c265f563ca329c6f
David W Millar <david.w.millar@gmail.com>
2024-12-26 20:07:41 -0500

    I read Day 2 part 2 wrong. Removing a bunch of cruft.

---
+++ src/day02/Day02.kt
@@ -0,0 +1,66 @@
+package day02
+
+import utils.readInput
+import java.util.concurrent.atomic.AtomicInteger
+
+/**
+ * [AoC 2024 - Day 2](https://adventofcode.com/2024/day/2)
+ */
+fun main() {
+
+    fun String.toListOfInts(regex: Regex = """\s+""".toRegex()) = trim().split(regex).map { it.toInt() }
+
+    fun List<Int>.containsDuplicates() = (toSet().size != size)
+
+    fun List<Int>.isStrictlyIncreasingByAtMost(maxStep: Int = 3) =
+        windowed(2,1).all {
+            (it.first() < it.last()).and(it.last() - it.first() <= maxStep)
+        }
+
+
+    /** Could use a better name... */
+    fun List<Int>.isStrictlyMonotonic(): Boolean {
+        if (containsDuplicates()) return false
+        val increasing = get(1) >= get(0) // Determine initial direction
+        for (i in 1 until (size - 1)) {
+            if (increasing && get(i + 1) < get(i)) {
+                return false
+            } else if (!increasing && get(i + 1) > get(i)) {
+                return false
+            }
+        }
+        return true
+    }
+
+    fun List<Int>.isSafeReport() =
+        isStrictlyMonotonic() &&
+            !containsDuplicates() &&
+            sorted().isStrictlyIncreasingByAtMost()
+
+    fun part1(lines: List<String>): Int {
+        val safeCount = AtomicInteger(0)
+        lines.forEach { line ->
+            line
+                .toListOfInts()
+                // TODO: use .count()
+                .run {
+                    if (isSafeReport()) {
+                        safeCount.getAndIncrement()
+                    }
+                }
+        }
+        return safeCount.get()
+    }
+
+    fun part2(input: List<String>) = "TODO"
+
+
+    println(":::")
+    println(part2(readInput("day02/Day02_test")))
+
+    println("::: Part1 Test: ${part1(readInput("day02/Day02_test"))}")
+    println("::: Part1     : ${part1(readInput("day02/Day02"))}")
+
+    println("::: Part2 Test: ${part1(readInput("day02/Day02_test"))}")
+    println("::: Part2     : ${part1(readInput("day02/Day02"))}")
+}
