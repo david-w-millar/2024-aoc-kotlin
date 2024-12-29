7554203fe07d1ef0502f4d7594cfa4007c706e22
David W Millar <david.w.millar@gmail.com>
2024-12-28 20:55:21 -0500

    Day 3 part 1 solution

--- src/day03/Day03.kt
+++ src/day03/Day03.kt
@@ -1,27 +1,50 @@
 package day03
 
 import utils.Day
+import utils.readInput
 
-
-/**
- * [AoC 2024 - Day 3](https://adventofcode.com/2024/day/2)
- */
 fun main() {
-    val day = Day(3, "test solution")
+    val day = Day(3, 161)
 
-    fun part1(lines: List<String>) {
-        println(lines)
+    val instructionRegex = """mul\((\d+),(\d+)\)""".toRegex()
+
+    fun getValidInstructions(lines: List<String>): List<String> {
+        return instructionRegex.findAll(lines.joinToString()).map { it.value }.toList()
     }
+
+    fun getMultiplicands(instruction: String) : Pair<Int,Int> {
+        val (x,y) =
+            instructionRegex
+                .matchEntire(
+                    instruction
+                )?.destructured ?: throw IllegalArgumentException("Incorrect input line $instruction")
+        return Pair(x.toInt(), y.toInt())
+    }
+
+    fun getMulResult(instruction: String) : Int {
+        return getMultiplicands(instruction).let { it.first * it.second }
+    }
+
+    fun part1(lines: List<String>): Int {
+        return getValidInstructions(lines).sumOf { getMulResult(it) }
+    }
+
     fun part2(lines: List<String>) {
         println(lines)
     }
 
-    day.run {
-        printWorkingSolutionAfterTest(part1(day.getInputLines()), part1(day.getTestInputLines()))
-        part2("part2 test solution").printWorkingSolutionAfterTest(
-            part2(day.getInputLines()),
-            part2(day.getTestInputLines())
-        )
-    }
+    check(part1(day.getTestInputLines()) == 161)
+    //println("::: Part 1 Solution: " + part1(day.getInputLines()))
 
+    val part2Input = readInput("src/day03/Day03_part2_test.txt")
+    println("::: Part 2 Test Solution: " + part2(part2Input))
+
+
+//    day.run {
+//        printWorkingSolutionAfterTest(part1(day.getInputLines()), part1(day.getTestInputLines()))
+//        part2("part2 test solution").printWorkingSolutionAfterTest(
+//            part2(day.getInputLines()),
+//            part2(day.getTestInputLines())
+//        )
+//    }
 }
