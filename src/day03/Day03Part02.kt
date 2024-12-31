0bfef1a1dccd63ddd1d4bbecd1518bbed503d7e9
David W Millar <david.w.millar@gmail.com>
2024-12-31 15:15:49 -0500

    More work on day 3 part 2

--- src/day03/Day03Part02.kt
+++ src/day03/Day03Part02.kt
@@ -1,56 +1,61 @@
 package day03
 
+import com.github.ajalt.mordant.rendering.TextColors.*
+import com.github.ajalt.mordant.rendering.TextStyles.*
+
 fun main() {
+    val exampleMulInput         = $$"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+    val exampleConditionalInput1 = $$"xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))}"
 
-    val exampleMulInput = """xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"""
-    val exampleConditionalInput = $$"xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))}"
-
-    val MUL_REGEX = """mul\((\d+),(\d+)\)""".toRegex()
-    val DONT_REGEX = """don't\(\)""".toRegex()
-    val DO_REGEX = """do\(\)""".toRegex()
-
-    fun getDontInstructions(input: String) = DONT_REGEX.findAll(input).toList()
-    fun getDoInstructions(input: String) = DO_REGEX.findAll(input).toList()
-    fun getMulInstructions(input: String) = MUL_REGEX.findAll(input).toList()
-
-    fun part2(
-        lines: List<String>,
-        debug: Boolean = false,
-    ): Int {
-        val input = lines.joinToString()
-        println("::: Input:                $input")
-        val dontMatches = getDontInstructions(input)
-        val doMatches = getDoInstructions(input)
-        val mulMatches = getMulInstructions(input)
-
-        println("::::: don't Matches")
-        dontMatches.forEachIndexed { i, mr ->
-            println("::: Match $i")
-            println(i)
-            println(mr)
-        }
-
-        println("::::: do Matches")
-        println(doMatches)
-
-        println("::::: Mul Matches")
-        println(mulMatches)
+    fun part2(input: String): Int {
+        val program = CorruptProgram(input)
+        println(program.printSexy())
+        println("-------------------------")
+        println(input)
+        println(program.sortedInstructions.map { it.value })
+        println("-------------------------")
 
         return 0
     }
 
-    fun part2(
-        input: String,
-        debug: Boolean = false,
-    ): Int = part2(listOf(input), debug)
+    fun part2(lines: List<String>) = part2(lines.joinToString())
+
 
 //    val part2TestInput = readInput("src/day03/Day03_part2_test.txt")
 //    check(part2(part2TestInput, debug = false) == 48)
 
     //println("::: Part 2 Test Solution: input: ${part2(input)}")
     println("\n\n::: Part 2 Test Solution: input: ${part2(exampleMulInput)}")
-    println("\n\n::: Part 2 Test Solution: input: ${part2(exampleConditionalInput)}")
+    println("\n\n::: Part 2 Test Solution: input: ${part2(exampleConditionalInput1)}")
+    println("\n\n::: Part 2 Test Solution: input: ${part2(exampleConditionalInput1)}")
 
+
+}
+
+class CorruptProgram(val input: String) {
+    val dontMatches = DONT_REGEX.findAll(input).toList()
+    val doMatches = DO_REGEX.findAll(input).toList()
+    val mulMatches = MUL_REGEX.findAll(input)
+
+    val sortedInstructions: List<MatchResult> =
+        dontMatches.toMutableList().let {
+            it.addAll(doMatches.toMutableList())
+            it.addAll(mulMatches)
+            it
+        }.sortedBy { it.range.start }
+
+    fun sexyOutput(): String =
+        input.replace(DONT_REGEX) { mr -> italic(red(mr.value)) }
+            .replace(DO_REGEX) { mr -> italic(blue(mr.value)) }
+            .replace(MUL_REGEX) { mr -> bold(green(mr.value)) }
+
+    fun printSexy() = println(sexyOutput())
+
+    companion object {
+        val MUL_REGEX = """mul\((\d{1,3}),(\d{1,3})\)""".toRegex()
+        val DONT_REGEX = """don't\(\)""".toRegex()
+        val DO_REGEX = """do\(\)""".toRegex()
+    }
 }
 
 //    fun getValidInstructions(lines: List<String>): List<String> = MULL_REGEX.findAll(lines.joinToString()).map { it.value }.toList()
