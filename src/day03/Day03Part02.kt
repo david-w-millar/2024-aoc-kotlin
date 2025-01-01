c48902e5695aa63768824e905ccf2b2de7017b08
David W Millar <david.w.millar@gmail.com>
2024-12-31 22:04:13 -0500

    Day3 Part 2 solution

--- src/day03/Day03Part02.kt
+++ src/day03/Day03Part02.kt
@@ -2,73 +2,83 @@ package day03
 
 import com.github.ajalt.mordant.rendering.TextColors.*
 import com.github.ajalt.mordant.rendering.TextStyles.*
+import day03.CorruptProgram.Companion.MUL_REGEX
+import utils.readInput
+import java.util.concurrent.atomic.AtomicInteger
+
+
 
 fun main() {
+    @Suppress("SpellCheckingInspection")
     val exampleMulInput         = $$"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+
+    @Suppress("SpellCheckingInspection")
     val exampleConditionalInput1 = $$"xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))}"
 
-    fun part2(input: String): Int {
-        val program = CorruptProgram(input)
-        println(program.printSexy())
-        println("-------------------------")
-        println(input)
-        println(program.sortedInstructions.map { it.value })
-        println("-------------------------")
+    // TODO: Type Alias?
+    fun MatchResult.isDo() = value == "do()"
+    fun MatchResult.isDont() = value == "don't()"
+    fun MatchResult.isMul() = MUL_REGEX.matches(value)
+    fun MatchResult.mulResult() = getMulValue(value)
 
-        return 0
+    fun part2(input: String, debug: Boolean = false): Int {
+        val cp = CorruptProgram(input)
+        if ( debug ) cp.prettyPrint()
+        if ( debug ) println(cp.instructions)
+
+        val sum = AtomicInteger(0)
+        var mulEnabled = true
+        cp.instructionMatches.forEach { mr ->
+            when {
+                mr.isDo() -> mulEnabled = true
+                mr.isDont() -> mulEnabled = false
+                mr.isMul() -> if( mulEnabled ) sum.getAndAdd(mr.mulResult())
+            }
+        }
+
+        return sum.toInt()
     }
 
-    fun part2(lines: List<String>) = part2(lines.joinToString())
+    fun part2(lines: List<String>, debug: Boolean = false) = part2(lines.joinToString(), debug)
 
+    // --- Solve the Problem
+    val part2TestInput = readInput("src/day03/Day03_part2_test.txt")
+    check(part2(part2TestInput) == 48)
+    println("::: Part 2 Test Solution: input: ${part2(exampleMulInput, true)}")
+    println("::: Part 2 Test Solution: input: ${part2(exampleConditionalInput1, true)}")
 
-//    val part2TestInput = readInput("src/day03/Day03_part2_test.txt")
-//    check(part2(part2TestInput, debug = false) == 48)
-
-    //println("::: Part 2 Test Solution: input: ${part2(input)}")
-    println("\n\n::: Part 2 Test Solution: input: ${part2(exampleMulInput)}")
-    println("\n\n::: Part 2 Test Solution: input: ${part2(exampleConditionalInput1)}")
-    println("\n\n::: Part 2 Test Solution: input: ${part2(exampleConditionalInput1)}")
-
+    println("\n::: Part 2 Solution: ${part2(readInput("src/day03/Day03.txt"))}")
 
 }
 
+fun getMulValue(instruction: String): Int {
+    val (x,y) =
+        MUL_REGEX
+            .matchEntire(instruction)
+            ?.destructured ?: throw IllegalArgumentException("Incorrect mul(x,y) instruction: $instruction")
+    return x.toInt() * y.toInt()
+}
+
+
 class CorruptProgram(val input: String) {
-    val dontMatches = DONT_REGEX.findAll(input).toList()
-    val doMatches = DO_REGEX.findAll(input).toList()
-    val mulMatches = MUL_REGEX.findAll(input)
+    val instructionMatches = (DONT_REGEX.findAll(input) + DO_REGEX.findAll(input) + MUL_REGEX.findAll(input))
+        .sortedBy { it.range.start }
+        .toList()
 
-    val sortedInstructions: List<MatchResult> =
-        dontMatches.toMutableList().let {
-            it.addAll(doMatches.toMutableList())
-            it.addAll(mulMatches)
-            it
-        }.sortedBy { it.range.start }
+    val instructions = instructionMatches.map { it.value }
+    private val pretty = lazy {
+        input.replace(DONT_REGEX) { mr -> italic(red(mr.value))  }
+            .replace(DO_REGEX)    { mr -> italic(blue(mr.value)) }
+            .replace(MUL_REGEX)   { mr -> bold(green(mr.value))  }
+    }
 
-    fun sexyOutput(): String =
-        input.replace(DONT_REGEX) { mr -> italic(red(mr.value)) }
-            .replace(DO_REGEX) { mr -> italic(blue(mr.value)) }
-            .replace(MUL_REGEX) { mr -> bold(green(mr.value)) }
-
-    fun printSexy() = println(sexyOutput())
+    fun prettyOutput(): String = pretty.value
+    fun prettyPrint() = println(prettyOutput())
 
     companion object {
         val MUL_REGEX = """mul\((\d{1,3}),(\d{1,3})\)""".toRegex()
         val DONT_REGEX = """don't\(\)""".toRegex()
         val DO_REGEX = """do\(\)""".toRegex()
     }
+
 }
-
-//    fun getValidInstructions(lines: List<String>): List<String> = MULL_REGEX.findAll(lines.joinToString()).map { it.value }.toList()
-//    fun getMultiplicands(instruction: String): Pair<Int,Int> {
-//        val (x,y) =
-//            mulInstructionRegex
-//                .matchEntire(
-//                    instruction
-//                )?.destructured ?: throw IllegalArgumentException("Incorrect input line $instruction")
-//        return Pair(x.toInt(), y.toInt())
-//    }
-//    fun getMulResult(instruction: String): Int = getMultiplicands(instruction).let { it.first * it.second }
-//    fun part1(lines: List<String>): Int = getValidInstructions(lines).sumOf { getMulResult(it) }
-
-//val mulInstructionRegex = """mul\((\d+),(\d+)\)""".toRegex()
-//val dontDoBlock = """don't\(\).*do\(\)""".toRegex()
