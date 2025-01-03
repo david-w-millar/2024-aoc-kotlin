43264eb409d5cfaae261adf49f8d9d76252209c0
David W Millar <david.w.millar@gmail.com>
2025-01-02 21:23:06 -0500

    Day 3 cleanup

--- src/day03/Day03Part02.kt
+++ src/day03/Day03Part02.kt
@@ -6,50 +6,41 @@ import day03.CorruptProgram.Companion.MUL_REGEX
 import utils.readInput
 import java.util.concurrent.atomic.AtomicInteger
 
-
-
 fun main() {
     @Suppress("SpellCheckingInspection")
-    val exampleMulInput         = $$"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+    val exampleMulInput = $$"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
 
     @Suppress("SpellCheckingInspection")
     val exampleConditionalInput1 = $$"xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))}"
 
-    // TODO: Type Alias?
-    fun MatchResult.isDo() = value == "do()"
-    fun MatchResult.isDont() = value == "don't()"
-    fun MatchResult.isMul() = MUL_REGEX.matches(value)
-    fun MatchResult.mulResult() = getMulValue(value)
-
     fun part2(input: String, debug: Boolean = false): Int {
         val cp = CorruptProgram(input)
-        if ( debug ) cp.prettyPrint()
-        if ( debug ) println(cp.instructions)
-
-        val sum = AtomicInteger(0)
-        var mulEnabled = true
-        cp.instructionMatches.forEach { mr ->
-            when {
-                mr.isDo() -> mulEnabled = true
-                mr.isDont() -> mulEnabled = false
-                mr.isMul() -> if( mulEnabled ) sum.getAndAdd(mr.mulResult())
-            }
+        if ( debug ) {
+            cp.prettyPrint()
+            println(cp.instructions)
         }
-
-        return sum.toInt()
+        return cp.getResult()
     }
 
     fun part2(lines: List<String>, debug: Boolean = false) = part2(lines.joinToString(), debug)
 
-    // --- Solve the Problem
+    // --- Tests
     val part2TestInput = readInput("src/day03/Day03_part2_test.txt")
     check(part2(part2TestInput) == 48)
     println("::: Part 2 Test Solution: input: ${part2(exampleMulInput, true)}")
     println("::: Part 2 Test Solution: input: ${part2(exampleConditionalInput1, true)}")
 
+    // --- Solve the Problem
     println("\n::: Part 2 Solution: ${part2(readInput("src/day03/Day03.txt"))}")
 
 }
+
+
+typealias Instruction = MatchResult
+fun Instruction.isDo() = value == "do()"
+fun Instruction.isDont() = value == "don't()"
+fun Instruction.isMul() = MUL_REGEX.matches(value)
+fun Instruction.mulResult() = getMulValue(value)
 
 fun getMulValue(instruction: String): Int {
     val (x,y) =
@@ -59,11 +50,23 @@ fun getMulValue(instruction: String): Int {
     return x.toInt() * y.toInt()
 }
 
-
 class CorruptProgram(val input: String) {
     val instructionMatches = (DONT_REGEX.findAll(input) + DO_REGEX.findAll(input) + MUL_REGEX.findAll(input))
         .sortedBy { it.range.start }
         .toList()
+
+    fun getResult(): Int {
+        val sum = AtomicInteger(0)
+        var mulEnabled = true
+        instructionMatches.forEach { i ->
+            when {
+                i.isDo() -> mulEnabled = true
+                i.isDont() -> mulEnabled = false
+                i.isMul() -> if( mulEnabled ) sum.getAndAdd(i.mulResult())
+            }
+        }
+        return sum.toInt()
+    }
 
     val instructions = instructionMatches.map { it.value }
     private val pretty = lazy {
