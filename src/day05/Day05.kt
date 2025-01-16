a286d22b67470ab1a6e653ff8125eca9a2d31e48
David W Millar <david.w.millar@gmail.com>
2025-01-16 10:52:28 -0500

    Day 05 work. Partial commit to pick things up elsewhere.

--- src/day05/Day05.kt
+++ src/day05/Day05.kt
@@ -1,18 +1,12 @@
 @file:Suppress(
     "ktlint:standard:function-signature",
     "ktlint:standard:class-signature",
+    "ktlint:standard:statement-wrapping",
     "unused",
 )
 
 package day05
 
-import com.github.ajalt.mordant.rendering.TextColors.blue
-import com.github.ajalt.mordant.rendering.TextColors.green
-import com.github.ajalt.mordant.rendering.TextColors.red
-import com.github.ajalt.mordant.rendering.TextStyles.bold
-import utils.printLines
-import utils.printSolution
-import utils.println
 import utils.readInput
 
 fun main() { part1() }
@@ -21,22 +15,50 @@ private val testInput = run { readInput("src/day05/Day05_test.txt") }
 private val input = run { readInput("src/day05/Day05.txt") }
 
 private fun part1() {
-    val inputRegex = """<?a>(\d+)\|<?b>(\d+)""".toRegex()
-    testInput.printLines()
+    val (rules, updates) = getRulesAndUpdates(testInput)
+    val u = updates.first()
+    val v = rules
 
-    val pair = inputRegex.matchEntire(testInput.first())
-    println(pair)
+}
 
-    testInput.filter { it.contains("|") }
-        .map {
-            println("JAWN")
-            val match = inputRegex.find(it)
-            match.groups.forEach { println(it) }
+typealias Page = List<Long>
+
+private data class Update(val updates: List<Long>) {
+    companion object {
+        fun fromString(s: String) = Update(s.split(",").map { it.trim().toLong() })
+        fun fromLines(lines: List<String>) = lines.filter { it.contains(",") }.map { Update.fromString(it) }
+    }
+}
+
+private data class Rule(val a: Long, val b: Long) {
+    constructor(vararg ab: Long) : this(ab.first(), ab.last()) {
+        check(ab.size == 2) { "Invalid rule.  Rules must consist of 2 page numbers.  Got ${ab.size} instead" }
+    }
+
+
+    fun appliesToUpdate(u: Update) =
+        listOf(a,b)
+            .all { u.updates.contains(it) }
+
+    fun checkAgainstUpdate(u: Update): Boolean {
+        u.updates.forEach {
+            if (it == a) return true
+            if (it == b) return false
         }
+        // Figure out how to get rid of this
+        return false
+    }
+
+    companion object {
+        fun fromString(s: String): Rule {
+            val (a, b) = s.split("|").map { it.toLong() }
+            return Rule(a.toLong(), b.toLong())
+        }
+        fun fromLines(lines: List<String>) = lines.filter { it.contains("|") }.map { Rule.fromString(it) }
+    }
 }
 
-data class Rule(val a: Long, val b: Long) {
-
+private fun getRulesAndUpdates(lines: List<String>): Pair<List<Rule>, List<Update>> {
+    val (ruleStrings, updateStrings) = testInput.filter { it.isNotEmpty() }.partition { it.contains("|") }
+    return Pair(Rule.fromLines(ruleStrings), Update.fromLines(updateStrings))
 }
-
-
