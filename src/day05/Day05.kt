30f29ea12efafb8682134016a00138977e496450
David W Millar <david.w.millar@gmail.com>
2025-01-16 21:57:56 -0500

    Day 05 Part 1 works on test input

--- src/day05/Day05.kt
+++ src/day05/Day05.kt
@@ -17,40 +17,56 @@ private val input = run { readInput("src/day05/Day05.txt") }
 private fun part1() {
     val (rules, updates) = getRulesAndUpdates(testInput)
 
-    println("::: Updates ${updates.size}")
-    val validUpdates = updates.filter {
-        it.satisfiesAllRules(rules)
-    }
-    println("::: Valid Updates ${validUpdates.size}")
-    validUpdates.forEach { println(it) }
+    println("::: All Rules (${rules.rules.size})")
+    println(rules)
 
-//    val u = updates.first()
-//    val ar = u.getApplicableRules(rules)
-//    println("::: Rules ${rules.size}")
-//    println("::: Input ${u}")
-//    println("::: Applicable Rules ${ar.size}")
-//    ar.forEach { println(it) }
-//
-//    println(u.satisfiesAllRules(ar))
+    val u = updates.first()
+    val ok = u.satisfiesAllRules(rules)
+    println(ok)
+
+    println("\n\n:::::::::::::::::::::\n\n")
+    val validUpdates = updates.filter { it.satisfiesAllRules(rules) }
+    println("::: Valid Updates (${validUpdates.size})")
+    validUpdates.forEach { println(it) }
 
 }
 
-typealias Page = List<Long>
-
 private data class Update(val updates: List<Long>) {
-    fun getApplicableRules(r: List<Rule>) = r.filter { it.appliesToUpdate(this) }
+    fun satisfiesAllRules(rules: Rules): Boolean {
+        println("::: Update")
+        println(this)
+        val applicableRules = rules.getApplicableRules(this)
 
-    fun satisfiesAllRules(r: List<Rule>): Boolean {
-        val applicable = getApplicableRules(r)
-        applicable.forEach {
-            if (!it.appliesToUpdate(this)) return false
+        println("\n::: Applicable Rules (${applicableRules.rules.size})")
+        applicableRules.rules.forEach { println(it) }
+
+        return applicableRules.rules.all {
+            val check = it.checkAgainstUpdate(this)
+            println("::: Checking rule: $it against $this - $check")
+            check
         }
-        return true
     }
+
+    override fun toString() = updates.joinToString()
 
     companion object {
         fun fromString(s: String) = Update(s.split(",").map { it.trim().toLong() })
         fun fromLines(lines: List<String>) = lines.filter { it.contains(",") }.map { Update.fromString(it) }
+    }
+}
+
+private data class Rules(val rules: List<Rule>) {
+    fun getApplicableRules(u: Update) = Rules(rules.filter { it.appliesToUpdate(u) })
+
+    override fun toString() =
+        buildString {
+            rules.forEach {
+                appendLine(it.toString())
+            }
+        }
+
+    companion object {
+        fun fromLines(lines: List<String>) = Rules(lines.filter { it.contains("|") }.map { Rule.fromString(it) })
     }
 }
 
@@ -59,10 +75,12 @@ private data class Rule(val a: Long, val b: Long) {
         check(ab.size == 2) { "Invalid rule.  Rules must consist of 2 page numbers.  Got ${ab.size} instead" }
     }
 
-
     fun appliesToUpdate(u: Update) =
-        listOf(a,b)
-            .all { u.updates.contains(it) }
+        u.updates
+            .containsAll(listOf(a,b))
+            .also { println(":: Update $u contains both $a and $b - $it") }
+
+    override fun toString() = "$a|$b"
 
     fun checkAgainstUpdate(u: Update): Boolean {
         require(u.updates.contains(a) && u.updates.contains(b))
@@ -74,11 +92,10 @@ private data class Rule(val a: Long, val b: Long) {
             val (a, b) = s.split("|").map { it.toLong() }
             return Rule(a.toLong(), b.toLong())
         }
-        fun fromLines(lines: List<String>) = lines.filter { it.contains("|") }.map { Rule.fromString(it) }
     }
 }
 
-private fun getRulesAndUpdates(lines: List<String>): Pair<List<Rule>, List<Update>> {
+private fun getRulesAndUpdates(lines: List<String>): Pair<Rules, List<Update>> {
     val (ruleStrings, updateStrings) = testInput.filter { it.isNotEmpty() }.partition { it.contains("|") }
-    return Pair(Rule.fromLines(ruleStrings), Update.fromLines(updateStrings))
+    return Pair(Rules.fromLines(ruleStrings), Update.fromLines(updateStrings))
 }
